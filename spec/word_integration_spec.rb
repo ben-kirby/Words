16352d4e6c70281724dc615c91fd0b8ba80d941f
Ben Kirby <benkirby@Bens-MacBook-Pro.local>
2018-11-30 11:34:48 -0800

    Has a passing integration spec for adding a new word

--- spec/word_integration_spec.rb
+++ spec/word_integration_spec.rb
@@ -6,3 +6,13 @@ require ('capybara')
 require ('./lib/word')
 Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
+
+describe('adding a new word', {:type => :feature}) do
+  it ('will add a new word to the page when the form is filled out') do
+    visit('/')
+    fill_in('new_word', :with=>'Dog')
+    fill_in('definition', :with=> "An animal")
+    click_button('Submit!')
+    expect(page).to have_content('Dog')
+  end
+end
