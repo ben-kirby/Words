38a2783791174f6d94b8607ecdc6c7aba5d25b8d
Ben Kirby <benkirby@Bens-MacBook-Pro.local>
2018-11-30 17:13:31 -0800

    Heroku testing

--- app.rb
+++ app.rb
@@ -4,25 +4,25 @@ also_reload './lib/**/*.rb'
 require './lib/word'
 require 'pry'
 
-get '/' do
+get ('/') do
   @list = Word.all()
   erb(:index)
 end
 
-post '/' do
+post ('/') do
   add_word = Word.new(params)
   add_word.save()
   @list = Word.all()
   erb(:index)
 end
 
-get '/word/:id' do
+get ('/word/:id') do
   id = params['id']
   @list = Word.find(id)
   erb(:index)
 end
 
-post '/word/:id' do
+post ('/word/:id') do
   id = params['id']
   @list = Word.find(id)
   @list.add_definition(params)
